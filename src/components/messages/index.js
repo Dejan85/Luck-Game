3713d461401a528da4e3ef13f3a8131a21fe08aa
unknown <xypnotica@gmail.com>
2019-07-21 17:17:57 +0200

    Izbacio sam p tag iz button-a

--- src/components/messages/index.js
+++ src/components/messages/index.js
@@ -1,3 +1,3 @@
 export const welcome = 'Dobro dosli u igre na srecu!';
 export const welcome2 =
-  'Popunite tiket. Treba da popunite 5 tiketa. Mozete izabrati minimalno 1 a maksimalno 5 brojeva. Svaki tiket kosta 100 $';
+  'Popunite tiket. Treba da popunite 5 tiketa. Mozete izabrati minimalno 1 a maksimalno 5 brojeva. Svaki tiket kosta 100$. Takodje mozete uloziti dodatni novac. Serecno!';
