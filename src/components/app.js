0b2098c3ac1bd34dce80a60118f405de6fd4003e
unknown <xypnotica@gmail.com>
2019-07-22 03:27:12 +0200

    ubacio da nestaju brojevi kada kliknemo na njih

--- src/components/app.js
+++ src/components/app.js
@@ -21,8 +21,12 @@ const app = function () {
   const ticketHandler = function () {
     if (allTickets.length !== 5) {
       if (ticket.length < 5) {
-        ticket.push(this.textContent);
-        btn.textContent = 'Potvrdi!';
+        if (this.textContent !== ' ') ticket.push(this.textContent);
+
+        // brisemo broj sa table kada kliknem na njega
+        this.textContent = ' ';
+
+        btn.textContent = 'POTVRDI!';
 
         // ubacujemo klinuti broj u izabrani brojevi
         [...selected__numbers.children].map((item, index) => {
@@ -39,6 +43,10 @@ const app = function () {
   const addTicketHandler = function () {
     if (allTickets.length !== 5) {
       showTicketHandler();
+      rollingGame();
+
+      // resetujemo brojeve na tabli
+      showFillTicket(ticketHandler);
       allTickets.push(ticket);
       ticket = [];
       [...selected__numbers.children].map(item => {
@@ -55,9 +63,28 @@ const app = function () {
     game__ticket.innerHTML += ticketTemplate(ticket);
   };
 
+  //
+  // ─── IZVLACENJE TIKETA ──────────────────────────────────────────────────────────
+  //
+
+  const rollingGame = function () {
+    if (allTickets.length === 4) {
+      btn.textContent = 'ODIGRAJ!';
+    }
+
+    if (btn.textContent === 'ODIGRAJ!') {
+      console.log('krenuli smo da igramo');
+    }
+  };
+
+  //
+  // ─── GLAVNO DUGME NA KOJE KLIKCEMO ──────────────────────────────────────────────
+  //
+
   btn.onclick = function () {
     if (btn.textContent === 'IGRAJ!') showFillTicket(ticketHandler);
-    if (btn.textContent === 'Potvrdi!') addTicketHandler();
+    if (btn.textContent === 'POTVRDI!') addTicketHandler();
+    if (btn.textContent === 'ODIGRAJ!') rollingGame();
   };
 };
 
--- src/components/messages/index.js
+++ src/components/messages/index.js
@@ -1,3 +1,7 @@
 export const welcome = 'Dobro dosli u igre na srecu!';
 export const welcome2 =
   'Popunite tiket. Treba da popunite 5 tiketa. Mozete izabrati minimalno 1 a maksimalno 5 brojeva. Svaki tiket kosta 100$. Takodje mozete uloziti dodatni novac. Serecno!';
+export const msg = broj => {
+  let text = `Popunjavate prvi tiket`;
+  return text;
+};
