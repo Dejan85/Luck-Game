5756111a0f659a30c9822567def5bb2906f8d83b
unknown <xypnotica@gmail.com>
2019-07-23 01:42:51 +0200

    uradio sam deo sa random biranjem brojeva

--- src/components/app.js
+++ src/components/app.js
@@ -8,18 +8,28 @@ import { showFillTicket, switchMessage } from './dom/functions';
 // templates
 import { ticketTemplate } from './dom/templates/template';
 
+// messages
+import { btnText, btnText2, btnText3 } from './messages';
+
 //
 // ─── MAIN FUN ───────────────────────────────────────────────────────────────────
 //
 
 const app = function () {
+  // Pojedinacni tiket array
   let ticket = [];
+  // Glavni array gde smestamo sve tikete
   let allTickets = [];
+  // Prikazuje pocetnu poruku dole na panelu
+  let combinations = [];
   switchMessage();
-  btn.textContent = 'IGRAJ!';
+  // Dodeljuje glavnom dugmetu text
+  btn.textContent = btnText;
 
   const ticketHandler = function () {
+    // Ogranicili smo koliko tiketa je maksimalno za popunjavanje
     if (allTickets.length !== 5) {
+      // Ogranicili smo koliko brojeva je max za izvlacenje
       if (ticket.length < 5) {
         // proveravamo prazno polje, ako je kliknuto na prazno polje nece ubaciti broj
         if (this.textContent !== ' ') ticket.push(this.textContent);
@@ -28,7 +38,7 @@ const app = function () {
         this.textContent = ' ';
 
         // menjamo text na glavnom dugmetu
-        btn.textContent = 'POTVRDI!';
+        btn.textContent = btnText2;
 
         // ubacujemo klinuti broj u izabrani brojevi
         [...selected__numbers.children].map((item, index) => {
@@ -43,8 +53,12 @@ const app = function () {
   //
 
   const addTicketHandler = function () {
+    // Proveravamo da li je svih 5 tiketa popunjeno
     if (allTickets.length !== 5) {
+      // Prikzuje svaki tiket pojedinacno
       showTicketHandler();
+
+      // Krece izvlacenje brojeva
       rollingGame();
 
       // resetujemo brojeve na tabli
@@ -68,6 +82,7 @@ const app = function () {
   //
 
   const showTicketHandler = function () {
+    // Ubacujemo tiket u html
     game__ticket.innerHTML += ticketTemplate(ticket);
   };
 
@@ -76,12 +91,51 @@ const app = function () {
   //
 
   const rollingGame = function () {
+    // Vrsimo proveru da li su svi tiketi popunjeni
     if (allTickets.length === 4) {
-      btn.textContent = 'ODIGRAJ!';
+      // Menjamo text glavnog dugmea
+      btn.textContent = btnText3;
+      // Smanjujemo font-size da bi text u glavnom dugmetu bio lepo prikazan
+      btn.style.fontSize = '2.3rem';
+      // Dodeljujemo glavnom dugmetu novi event
+      btn.onclick = playGameHandler;
     }
+  };
 
-    if (btn.textContent === 'ODIGRAJ!') {
-      console.log('krenuli smo da igramo');
+  // Funkcija koja upravlja izvlacenjem brojeva
+  const playGameHandler = function () {
+    // Kreiramo arr sa 30 brojeva koji ce nam sluziti kao brojcanik
+    const arr = Array.apply(this, Array(30)).map((item, index) => index + 1);
+
+    // Interval koji ce nam izvlaciti na svake 2 sekunde po jedan broj
+    const interval = setInterval(getRandomNum, 500);
+    // Counter koji nam sluzi da interval zna kada da se zaustavi
+    let counter = 0;
+
+    // Funkcija koja vraca random broj
+    function getRandomNum () {
+      // Counter se povecava za 1 i kada dodje do 12 interval ce prestati da radi
+      counter++;
+
+      if (counter <= 12) {
+        // Ovde generesimo random broj funkcijom random()
+        let num = random();
+        // Ovde ubacujemo generisan broj u glavni array
+        combinations.push(num);
+      } else {
+        // Kada je izvuceno 12 brojeva interval prestaje sa radom
+        window.clearInterval(interval);
+      }
+
+      // Funkcija koja nam vraca random broj
+      function random () {
+        // Generisemo random broj tako sto arr-u dodajemo kao index random broj i tako dobijamo element iz ovog array-a
+        let randNum = arr[Math.floor(Math.random() * arr.length)];
+        // Ovde brisemo dobijeni element iz arr-a iz razloga da ga u sledecem krugu nebi dobili kao duplikat
+        arr.splice(arr.indexOf(randNum), 1);
+
+        return randNum;
+      }
     }
   };
 
@@ -90,9 +144,12 @@ const app = function () {
   //
 
   btn.onclick = function () {
-    if (btn.textContent === 'IGRAJ!') showFillTicket(ticketHandler);
-    if (btn.textContent === 'POTVRDI!') addTicketHandler();
-    if (btn.textContent === 'ODIGRAJ!') rollingGame();
+    // Prikazuje tablu sa koje briramo brojeve
+    if (btn.textContent === btnText) showFillTicket(ticketHandler);
+    // Pokrece funkciju koja sluzi za popunjavanje i cekiranje tiketa
+    if (btn.textContent === btnText2) addTicketHandler();
+    // Pokrece funkciju koja sluzi za izvlacenje brojeva
+    if (btn.textContent === btnText3) rollingGame();
   };
 };
 
--- src/components/messages/index.js
+++ src/components/messages/index.js
@@ -1,7 +1,15 @@
+// welcome msg
 export const welcome = 'Dobro dosli u igre na srecu!';
 export const welcome2 =
   'Popunite tiket. Treba da popunite 5 tiketa. Mozete izabrati minimalno 1 a maksimalno 5 brojeva. Svaki tiket kosta 100$. Takodje mozete uloziti dodatni novac. Serecno!';
+
+// playing msg
 export const msg = broj => {
   let text = `Popunjavate prvi tiket`;
   return text;
 };
+
+// btns
+export const btnText = 'IGRAJ!';
+export const btnText2 = 'DODAJ!';
+export const btnText3 = 'POCNI IZVLACENJE!';
