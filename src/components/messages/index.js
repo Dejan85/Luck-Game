953fe94d90a7ccd415ac8e9b9ee0740ba999cc18
unknown <xypnotica@gmail.com>
2019-07-28 21:40:09 +0200

    igra je gotova

--- src/components/messages/index.js
+++ src/components/messages/index.js
@@ -1,18 +1,18 @@
 // playing msg
 export const msg = (param, param2) => {
   if (param === 1) {
-    return 'Dobro dosli u igre na srecu!';
+    return 'Dobro došli u igre na sreću!';
   } else if (param === 2) {
-    return 'Popunite tiket. Treba da popunite 5 tiketa. Mozete izabrati minimalno 1 a maksimalno 5 brojeva. Svaki tiket kosta 100$. Takodje mozete uloziti dodatni novac.';
+    return 'Popunite tiket. Treba da popunite 5 tiketa. Možete izabrati minimalno 1 a maksimalno 5 brojeva. Svaki tiket košta 100$. Takođe možete uložiti dodatni novac.';
   } else if (param === 3) {
-    return 'Pocinje izvlacenje. Srecno!';
+    return 'Počinje izvlačenje. Srećno!';
   } else if (param === 4) {
     // return `Izvlacenje je zavrseno. Osvojili ste ukupno ${param2} $. Mozete igrati ponovo sa istim tiketima, ili mozete popuniti nove. Vise srece na sledecem izvlacenju!`;
     return `Osvojili ste ukupno ${param2} $`;
   } else if (param === 5) {
     return 'Unesite ulog i pritisnite enter.';
   } else if (param === 6) {
-    return `Ulozili ste ${param2} $`;
+    return `Uložili ste ${param2} $`;
   } else if (param === 7) {
     return 'Nemate toliko novca';
   }
@@ -21,5 +21,5 @@ export const msg = (param, param2) => {
 // btns
 export const btnText = 'IGRAJ!';
 export const btnText2 = 'DODAJ!';
-export const btnText3 = 'POCNI IZVLACENJE!';
-export const btnText4 = 'IZVLACI PONOVO';
+export const btnText3 = 'POČNI IZVLAČENJE!';
+export const btnText4 = 'IZVLAČI PONOVO';
