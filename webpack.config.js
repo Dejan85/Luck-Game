9f208bfbdb45277c38ff2cc0ee655564b1349c79
unknown <xypnotica@gmail.com>
2019-07-28 23:52:50 +0200

    heroku deploy

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,7 @@ const HtmlWebPackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
+  mode: 'production',
   module: {
     rules: [
       // babel rules
